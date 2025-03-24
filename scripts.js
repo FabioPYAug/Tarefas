7ef33b4e8f6438d3533fe829ce3222b56eca40d0
ssehnem <sarasehnem84@gmail.com>
2025-03-24 10:36:16 -0300

    test: inserindo elementos

---
+++ scripts.js
@@ -0,0 +1,4 @@
+var taskInput = document.getElementById("new-task");
+var addButton = document.getElementsByTagName("button")[0];
+var incompleteTasksHolder = document.getElementById("incomplete-tasks");
+var completedTasksHolder = document.getElementById("completed-tasks");
