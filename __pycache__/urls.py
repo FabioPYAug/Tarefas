44f902a78e05a4b0d74061b01327cd54a735fb13
Henrique Costa <hencostnascimento@gmail.com>
2025-03-27 09:53:21 -0300

    build: Configuração do Django

---
+++ __pycache__/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.http import JsonResponse
+
+def api_root(request):
+    return JsonResponse({
+        "endpoints": {
+            #"questions": "/api/questions/",
+            #"categories": "/api/categories/"
+        }
+    })
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('tarefas.urls')), 
+    path('', api_root), 
+]
