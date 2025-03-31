d1bb28bb307dea31a953ecdada4dbd1025a82259
Ana Laura <analaurasilvamuller@gmail.com>
2025-03-31 09:05:02 -0300

    build: alteração modo noturno

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -2,3 +2,22 @@ var taskInput = document.getElementById("InputTarefa");
 var addButton = document.getElementsByTagName("button")[0];
 var incompleteTasksHolder = document.getElementById("ListaFazer");
 var completedTasksHolder = document.getElementById("ListaFeita");
+
+document.addEventListener("DOMContentLoaded", function () {
+    var botaoNoturno = document.getElementById("modonoturno");
+    var body = document.body;
+
+    if (localStorage.getItem("modoNoturno") === "ativado") {
+        body.classList.add("dark-mode");
+    }
+
+    botaoNoturno.addEventListener("click", function () {
+        body.classList.toggle("dark-mode");
+
+        if (body.classList.contains("dark-mode")) {
+            localStorage.setItem("modoNoturno", "ativado");
+        } else {
+            localStorage.setItem("modoNoturno", "desativado");
+        }
+    });
+});
