c5f9716a49b9de9283564ba503c9d1afe5341073
ssehnem <sarasehnem84@gmail.com>
2025-04-03 10:17:30 -0300

    fix: django

--- frontend/listas.js
+++ frontend/listas.js
@@ -91,3 +91,26 @@ async function addNewTask() {
         }
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    var botaoNoturno = document.getElementById("modonoturno");
+    var icone = document.getElementById("iconeModo");
+    var body = document.body;
+
+    if (localStorage.getItem("modoNoturno") === "ativado") {
+        body.classList.add("dark-mode");
+        icone.classList.replace("fa-moon", "fa-sun");
+    }
+
+    botaoNoturno.addEventListener("click", function () {
+        body.classList.toggle("dark-mode");
+
+        if (body.classList.contains("dark-mode")) {
+            localStorage.setItem("modoNoturno", "ativado");
+            icone.classList.replace("fa-moon", "fa-sun");
+        } else {
+            localStorage.setItem("modoNoturno", "desativado");
+            icone.classList.replace("fa-sun", "fa-moon");
+        }
+    });
+});
